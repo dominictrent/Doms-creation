1c10675f38a147f20bc96df41988fbed87c51ee8
dominictrent <Dominctrent@icloud.com>
2022-12-20 11:23:50 -0700

    im

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // JavaScript Code
+const helmet = document.getElementById("helmet");
+const colorSelect = document.getElementById("color");
+const decalSelect = document.getElementById("decal");
+
+function updateHelmet() {
+  helmet.style.backgroundColor = colorSelect.value;
+  helmet.style.backgroundImage = `url(decals/${decalSelect.value}.png)`;
+}
+
+colorSelect.addEventListener("change", updateHelmet);
+decalSelect.addEventListener("change", updateHelmet);
+
+updateHelmet();
