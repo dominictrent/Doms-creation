f375b3cd60e77fa1b9fb005f8f8e8e1918ef6caa
dominictrent <Dominctrent@icloud.com>
2022-12-22 11:20:23 -0700

    yes

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
-// Get all the options elements
+const customizerForm = document.getElementById("customizer");
 const colorOptions = document.querySelectorAll(".color-options input");
 const graphicsOptions = document.querySelectorAll(".graphics-options input");
 const visorOptions = document.querySelectorAll(".visor-options input");
-const sizeOptions = document.querySelectorAll(".size-options input");
 
-// Add event listeners to the options
-colorOptions.forEach((option) =>
-  option.addEventListener("change", updatePreview)
-);
-graphicsOptions.forEach((option) =>
-  option.addEventListener("change", updatePreview)
-);
-visorOptions.forEach((option) =>
-  option.addEventListener("change", updatePreview)
-);
-sizeOptions.forEach((option) =>
-  option.addEventListener("change", updatePreview)
-);
+customizerForm.addEventListener("change", (event) => {
+  const target = event.target;
+  if (target.matches(".color-options input")) {
+    handleColorChange(target.value);
+  } else if (target.matches(".graphics-options input")) {
+    handleGraphicsChange(target.value);
+  } else if (target.matches(".visor-options input")) {
+    handleVisorChange(target.value);
+  }
+});
 
-// Update the preview image based on the selected options
-function updatePreview() {
-  // Get the selected options
-  const color = getSelectedOption(colorOptions);
-  const graphics = getSelectedOption(graphicsOptions);
-  const visor = getSelectedOption(visorOptions);
-  const size = getSelectedOption(sizeOptions);
-
-  // Update the preview image with the selected options
-  const preview = document.querySelector(".preview img");
-  preview.src = `/images/helmets/${color}-${graphics}-${visor}-${size}.png`;
+function handleColorChange(color) {
+  // Update the color of the object based on the selected option
+  console.log(`Color changed to: ${color}`);
 }
 
-// Get the value of the selected option in a group of options
-function getSelectedOption(options) {
-  for (let i = 0; i < options.length; i++) {
-    if (options[i].checked) {
-      return options[i].value;
-    }
-  }
+function handleGraphicsChange(graphics) {
+  // Update the graphics of the object based on the selected option
+  console.log(`Graphics changed to: ${graphics}`);
+}
+
+function handleVisorChange(visor) {
+  // Update the visor of the object based on the selected option
+  console.log(`Visor changed to: ${visor}`);
 }
