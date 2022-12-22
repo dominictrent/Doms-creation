5e6f503291f0613f4936ad9564f0fb8ae3da349d
dominictrent <Dominctrent@icloud.com>
2022-12-22 10:07:05 -0700

    what

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 // JavaScript Code
 const helmet = document.getElementById("helmet");
-const colorSelect = document.getElementById("color");
-const decalSelect = document.getElementById("decal");
+const base = document.getElementById("base");
+const faceMask = document.getElementById("face-mask");
+const shell = document.getElementById("shell");
+const decals = document.getElementById("decals");
+const leftSide = document.getElementById("left-side");
+const rightSide = document.getElementById("right-side");
+
+// Customization options
+const colors = ["#fff", "#000", "#f00", "#0f0", "#00f"];
+const patterns = ["solid", "striped", "dotted", "checkered"];
+const decalImages = ["image1.png", "image2.png", "image3.png"];
+const decalElement = document.getElementById("my-decal");
+decalElement.addEventListener("click", function () {
+  // Get the decal image source
+  const decal = this.src;
+
+  // Update the helmet decal
+  document.getElementById("left-side").style.backgroundImage = `url(${decal})`;
+  document.getElementById("right-side").style.backgroundImage = `url(${decal})`;
+});
+
+// Set default customization options
+let color = "#fff";
+let pattern = "solid";
+let decal = "image1.png";
 
 function updateHelmet() {
-  helmet.style.backgroundColor = colorSelect.value;
-  helmet.style.backgroundImage = `url(decals/${decalSelect.value}.png)`;
+  base.style.backgroundColor = color;
+  faceMask.style.backgroundColor = color;
+  shell.style.backgroundColor = color;
+
+  if (pattern === "striped") {
+    base.style.backgroundImage = "linear-gradient(#fff, #fff)";
+    faceMask.style.backgroundImage = "linear-gradient(#fff, #fff)";
+    shell.style.backgroundImage = "linear-gradient(#fff, #fff)";
+  }
 }
-
-colorSelect.addEventListener("change", updateHelmet);
-decalSelect.addEventListener("change", updateHelmet);
-
-updateHelmet();
